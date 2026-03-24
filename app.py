77ab195f9cd556645da2c8a58dfbe63f5f6f448d
Sumit Rajoria <sumitrajoria@Whiskey.local>
2026-03-24 14:27:09 +0530

    initial commit

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ app.config["SECRET_KEY"] = "your_secret_key_here"
 # app.config["SQLALCHEMY_DATABASE_URI"] = (
 #     "postgresql://postgres:Nopassword%4003@localhost/test"
 # )db_url = os.environ.get("DATABASE_URL")
-idb_url = os.environ.get("DATABASE_URL")
+db_url = os.environ.get("DATABASE_URL")
 
 if db_url and db_url.startswith("postgres://"):
     db_url = db_url.replace("postgres://", "postgresql://", 1)
